50497105a4877eaa7aa8317d013e284ceb20f6d7
SameetPathan <sameetpathanrs@gmail.com>
2023-12-26 02:29:29 +0530

    v1

---
+++ src/contracts/Driver.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract DriverContract {
+
+    struct DriverData {
+        address DriverID;
+        string DriverName;
+        uint Experience;
+        string PhoneNumber;
+        string Address;
+        string CarRegisterNumber;
+        string CarName;
+        string LicenseNumber;
+        string CurrentLocation;
+        uint status;
+    }
+
+    DriverData[] public Drivers;
+
+    function addDriver(
+        address DriverID,
+        string memory DriverName,
+        uint Experience,
+        string memory PhoneNumber,
+        string memory Address,
+        string memory CarRegisterNumber,
+        string memory CarName,
+        string memory LicenseNumber,
+        string memory CurrentLocation,
+        uint status
+    ) public {
+        DriverData memory d = DriverData(
+            DriverID,
+            DriverName,
+            Experience,
+            PhoneNumber,
+            Address,
+            CarRegisterNumber,
+            CarName,
+            LicenseNumber,
+            CurrentLocation,
+            status
+        );
+        Drivers.push(d);
+    }
+
+    function updateDriver(address DriverID, uint status) public {
+        for (uint i = 0; i < Drivers.length; i++) {
+            if (Drivers[i].DriverID == DriverID) {
+                Drivers[i].status = status;
+                return;
+            }
+        }
+    }
+
+    function getDriver(address DriverID) public view returns (
+        string memory,
+        string memory,
+        uint,
+        string memory,
+        string memory,
+        string memory,
+        string memory,
+        string memory,
+        uint
+    ) {
+        for (uint i = 0; i < Drivers.length; i++) {
+            if (Drivers[i].DriverID == DriverID) {
+                return (
+                    Drivers[i].DriverName,
+                    Drivers[i].PhoneNumber,
+                    Drivers[i].Experience,
+                    Drivers[i].Address,
+                    Drivers[i].CarRegisterNumber,
+                    Drivers[i].CarName,
+                    Drivers[i].LicenseNumber,
+                    Drivers[i].CurrentLocation,
+                    Drivers[i].status
+                );
+            }
+        }
+
+        return ("Not Found", "Not Found", 0, "Not Found", "Not Found", "Not Found", "Not Found", "Not Found", 0);
+    }
+
+    function getStatus(address DriverID) public view returns (uint) {
+        for (uint i = 0; i < Drivers.length; i++) {
+            if (Drivers[i].DriverID == DriverID) {
+                return Drivers[i].status;
+            }
+        }
+        return 0;
+    }
+
+    function getAllDrivers() public view returns (DriverData[] memory) {
+        return Drivers;
+    }
+
+    function getNumberOfRecords() public view returns (uint) {
+        return Drivers.length;
+    }
+}
